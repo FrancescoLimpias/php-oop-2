4337548a8f08d5d2a9a82b36799a1f4c10e9bf3a
Francesco Limpias <francescolimpias@gmail.com>
2023-01-28 19:09:24 +0100

    Added discount logic (mock expiry date)

--- Models/Product.php
+++ Models/Product.php
@@ -6,6 +6,10 @@ class Product
     // Identity
     private $name, $price, $description;
     private Category $category;
+    // Optional
+    const EXP_DISCOUNT = 30;
+    const MAX_DISCOUNT = 90;
+    private $expDate, $discount;
 
     // Constructor
     public function __construct($name, $price, $description, Category $category)
@@ -53,6 +57,37 @@ class Product
         $this->category = $category;
     }
 
+    public function getExpDate()
+    {
+        return isset($this->expDate) ? $this->expDate : false;
+    }
+    public function setExpDate($expDate)
+    {
+        $this->expDate = $expDate;
+    }
+
+    public function getDiscount()
+    {
+        // expiration discount
+        $expDiscount =
+            $this->getExpDate() /* expDate has been set */
+            && true /* expiration delta */ ?
+            30 : 0;
+
+        return max($expDiscount, $this->discount);
+    }
+    public function setDiscount($discount)
+    {
+        if (
+            is_int($discount)
+            && ($discount > self::MAX_DISCOUNT || $discount < 0)
+        ) {
+            throw new Exception("Invalid Argument: discount is not a valid integer!");
+        }
+        
+        $this->discount = $discount;
+    }
+
     // Utils
     public function getHTML()
     {
@@ -60,6 +95,8 @@ class Product
             "name: " . $this->getName() .
             "<br>" . "price: " . $this->getPrice() .
             "<br>" . "description: " . $this->getDescription() .
-            "<br>" . "category: " . $this->getCategory()->getHTML();
+            "<br>" . "category: " . $this->getCategory()->getHTML() .
+            "<br>" . "exp date: " . $this->getExpDate() ? $this->getExpDate() : "no expiration date" .
+            "<br>" . "discount: " . $this->getDiscount() . "%";
     }
 }
